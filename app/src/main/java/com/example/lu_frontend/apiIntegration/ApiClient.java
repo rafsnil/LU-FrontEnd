454f746e418541fd6539993d56d103ed479bf53f
rafsnil <rafsnil@gmail.com>
2024-06-18 22:01:55 +0600

    agp changed

--- app/src/main/java/com/example/lu_frontend/apiIntegration/ApiClient.java
+++ app/src/main/java/com/example/lu_frontend/apiIntegration/ApiClient.java
@@ -9,7 +9,7 @@ public class ApiClient {
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("https://6298-103-58-74-251.ngrok-free.app")
+                    .baseUrl("https://818f3-103-58-74-251.ngrok-free.app")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
